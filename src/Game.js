c2856b128f88089cfa7ae8b95c214a0e5b4f302e
Alexey Korepanov <alexkorep@gmail.com>
2019-01-12 22:29:49 +0100

    Fix opponen live

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,11 @@
 PrinceJS.Game = function (game) {
 
     this.kid;
-    
+
     this.level;
-    
+
     this.ui;
-    
+
     this.guards = [];
 
 };
@@ -13,18 +13,18 @@ PrinceJS.Game = function (game) {
 PrinceJS.Game.prototype = {
 
 	preload: function () {
-       
+
         this.load.json('level','assets/maps/level' + PrinceJS.currentLevel + '.json');
-        
+
     },
-    
+
     create: function () {
 
-         
+
         var json = this.game.cache.getJSON('level');
-        
+
         this.level = new PrinceJS.LevelBuilder(this.game).buildFromJSON(json);
-        
+
         for ( var i = 0; i < json.guards.length ; i++ ) {
 
             var ginfo = json.guards[i];
@@ -32,59 +32,58 @@ PrinceJS.Game.prototype = {
             this.guards.push(guard);
 
         }
-        
+
         json.prince.room = 2;
         json.prince.location = 19;
-        
+
         this.kid = new PrinceJS.Kid(this.game,this.level,json.prince.location,json.prince.direction,json.prince.room);
-        
+
         this.kid.onChangeRoom.add(this.setupCamera, this);
         this.kid.onChangeRoom.add(this.checkForOpponent, this);
         this.kid.onNextLevel.add(this.nextLevel, this);
-        
-        
+
+
         this.setupCamera(json.prince.room);
-        
+
         this.world.sort('z');
-        
+
         this.world.alpha = 1;
-        
+
         this.ui = new PrinceJS.Interface(this.game);
         this.ui.setPlayerLive(this.kid);
-        this.ui.setOpponentLive(this.guards[1]);
-        
+
         this.game.time.events.loop(80, this.updateWorld, this);
-        
+
         // for level 1 only -> close gate first room !!!
         if ( PrinceJS.currentLevel == 1 ) this.level.fireEvent(8,PrinceJS.Level.TILE_DROP_BUTTON);
-        
+
         this.input.keyboard.addKey(Phaser.Keyboard.Q).onDown.add(this.previousLevel, this);
         this.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(this.nextLevel, this);
         this.input.keyboard.addKey(Phaser.Keyboard.R).onDown.add(this.reset, this);
-        
+
 	},
 
-   
+
 	update: function () {
 
-        
+
 
 	},
-    
+
     updateWorld: function () {
-        
+
         this.level.update();
         this.kid.updateActor();
         for ( var i = 0; i < this.guards.length ; i++ ) {
-            
+
             this.guards[i].updateActor();
-            
+
         }
         this.ui.updateUI();
-        
+
     },
-    
-    
+
+
 	nextLevel: function () {
 
 		PrinceJS.currentLevel++;
@@ -92,7 +91,7 @@ PrinceJS.Game.prototype = {
         this.reset();
 
 	},
-    
+
     previousLevel: function () {
 
 		PrinceJS.currentLevel--;
@@ -100,43 +99,44 @@ PrinceJS.Game.prototype = {
         this.reset();
 
 	},
-    
+
     reset: function () {
-     
+
         this.guards = [];
-        
+
         if ( [2,4,6,8,9,12,15].indexOf(PrinceJS.currentLevel) > -1 ) {
-            
+
             this.state.start('Cutscene');
-            
+
         } else {
-        
+
             this.state.start('Game');
-        
+
         }
-        
+
     },
-    
+
     setupCamera: function(room) {
-      
+
         this.game.camera.x = this.level.rooms[room].x * PrinceJS.SCREEN_WIDTH * PrinceJS.SCALE_FACTOR;
         this.game.camera.y = this.level.rooms[room].y * PrinceJS.ROOM_HEIGHT * PrinceJS.SCALE_FACTOR;
-        
+
     },
-    
+
     checkForOpponent: function(room) {
-        
+
         for ( var i = 0; i < this.guards.length ; i++ ) {
-            
+
             if ( ( this.guards[i].room == room ) && this.guards[i].alive ) {
-                
+
                 this.kid.opponent = this.guards[i];
                 this.guards[i].opponent = this.kid;
-                
+                this.ui.setOpponentLive(this.guards[i]);
+
             }
-            
+
         }
-        
+
     }
 
 };
--- src/Interface.js
+++ src/Interface.js
@@ -1,114 +1,114 @@
 PrinceJS.Interface = function (game) {
 
     this.game = game;
-    
+
     this.text;
-    
+
     var bmd = this.game.make.bitmapData(PrinceJS.SCREEN_WIDTH, PrinceJS.UI_HEIGHT);
     bmd.fill(0,0,0);
-        
+
     this.layer = this.game.add.sprite(0, (PrinceJS.SCREEN_HEIGHT - PrinceJS.UI_HEIGHT) * PrinceJS.SCALE_FACTOR, bmd);
     this.layer.fixedToCamera = true;
-    
+
     this.text = this.game.make.bitmapText(PrinceJS.SCREEN_WIDTH * 0.5, (PrinceJS.UI_HEIGHT - 1) * 0.5, 'font','',16);
     this.text.setText('LEVEL ' + PrinceJS.currentLevel);
     this.text.anchor.setTo(0.5,0.5);
-    
+
     this.layer.addChild(this.text);
-    
+
     this.playerHPs = [];
     this.oppHPs = [];
-    
+
     this.playerHPActive;
     this.oppHPActive;
-    
+
 };
 
 PrinceJS.Interface.prototype = {
 
     setPlayerLive: function(actor) {
-        
+
         this.playerHPActive = actor.health;
         for (var i=0; i < actor.health; i++) {
-         
+
             this.playerHPs[i] =  this.game.add.sprite(i*7,2,'general','kid-live');
             this.layer.addChild(this.playerHPs[i]);
-            
+
         }
-        
+
         actor.onDamageLive.add(this.damagePlayerLive, this);
         actor.onRecoverLive.add(this.recoverPlayerLive, this);
         actor.onAddLive.add(this.addPlayerLive, this);
-        
+
     },
-    
+
     damagePlayerLive: function(num) {
-     
+
         for (var i=0; i<num; i++) {
-        
+
             this.playerHPActive--;
             this.playerHPs[this.playerHPActive].frameName = 'kid-emptylive';
-            
+
         }
-        
+
     },
-    
+
     recoverPlayerLive: function() {
-        
+
         this.playerHPs[0].frameName = 'kid-live';
         this.playerHPs[this.playerHPActive].frameName = 'kid-live';
         this.playerHPActive++;
-        
+
     },
-    
+
     addPlayerLive: function() {
-     
+
         var num = this.playerHPs.length;
         var hp = this.game.add.sprite(num*7,2,'general','kid-emptylive');
         this.playerHPs[num] = hp;
         this.layer.addChild(this.playerHPs[num]);
-        
+
     },
-    
+
     setOpponentLive: function(actor) {
-        
-        if (actor.charName == 'skeleton') return;
-        
+
+        if (!actor || actor.charName == 'skeleton') return;
+
         this.oppHPActive = actor.health;
         for (var i=actor.health; i > 0; i--) {
-         
+
             this.oppHPs[i - 1] =  this.game.add.sprite(PrinceJS.SCREEN_WIDTH - i*7 + 1,2,'general', actor.charName + '-live');
             this.layer.addChild(this.oppHPs[i - 1]);
-            
+
         }
-        
+
         actor.onDamageLive.add(this.damageOpponentLive, this);
-        
+
     },
-    
+
     damageOpponentLive: function() {
-     
+
         this.oppHPActive--;
         this.oppHPs[this.oppHPActive].visible = false;
-        
+
     },
-    
+
     updateUI: function() {
-        
+
         if (this.playerHPActive == 1) {
-            
+
             if (this.playerHPs[0].frameName == 'kid-live') {
-                
+
                 this.playerHPs[0].frameName = 'kid-emptylive';
-                
+
             } else {
-                
+
                 this.playerHPs[0].frameName = 'kid-live';
-                
+
             }
-            
+
         }
-        
+
     }
 
 };
